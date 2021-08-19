96e45c351c12962ab75c3e56ab7e5b4f6f4e1353
YadavGulshan <yadavgulshan542@gmail.com>
2021-08-19 20:46:48 +0530

    Cleanup

--- lib/services/geolocation.dart
+++ lib/services/geolocation.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-
 import 'package:cuisine_app/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
 
 class LocationService extends StatefulWidget {
   const LocationService({Key? key}) : super(key: key);
@@ -52,7 +49,7 @@ class LocationServiceState extends State<LocationService> {
         await placemarkFromCoordinates(postition.latitude, postition.longitude);
 
     Placemark placemark = newPlace[1];
-    debugPrint("########Placemark: " + placemark.toString());
+    // debugPrint("########Placemark: " + placemark.toString());
     // Set the lon and lat
     setState(() {
       latitude = postition.latitude;
