0ca4a625a316bd48b5a20ba4442835db7d54ceea
YadavGulshan <yadavgulshan542@gmail.com>
2021-08-26 21:15:49 +0530

    change package name

--- android/app/build/generated/source/buildConfig/debug/com/example/cuisine/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/example/cuisine/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.example.cuisine_app;
+package com.example.cuisine;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.cuisine_app";
+  public static final String APPLICATION_ID = "com.example.cuisine";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
--- android/app/src/main/kotlin/com/example/cuisine/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/cuisine/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cuisine_app
+package com.example.cuisine
 
 import io.flutter.embedding.android.FlutterActivity
 
