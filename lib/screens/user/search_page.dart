9e5747d0487c9eff5e5da1a29073d9fe6840c1de
YadavGulshan <yadavgulshan542@gmail.com>
2021-08-28 23:33:00 +0530

    fixed some ui issues

--- lib/screens/user/search_page.dart
+++ lib/screens/user/search_page.dart
@@ -11,30 +11,35 @@ class SearchPage extends StatelessWidget {
       children: [
         SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(14.0),
+            padding: const EdgeInsets.all(18.0),
             child: Container(
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
+                  borderRadius: BorderRadius.circular(17),
+                  border: Border.all(color: primaryColor),
                   color: (appTheme == Brightness.light)
                       ? Colors.white
                       : Colors.grey[800],
                   boxShadow: const [BoxShadow(blurRadius: 0.4)]),
-              height: MediaQuery.of(context).size.height * 0.070,
+              height: MediaQuery.of(context).size.height * 0.062,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   IconButton(
                     onPressed: () {
                       Navigator.pop(context);
                     },
-                    icon: const Icon(Icons.arrow_back_ios),
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      size: 20,
+                    ),
                   ),
                   Flexible(
+                    fit: FlexFit.tight,
                     child: TextFormField(
                       enabled: true,
                       autofocus: true,
-                      cursorHeight: 25,
-                      style: const TextStyle(fontSize: 22),
+                      // cursorHeight: 25,
+                      style: const TextStyle(fontSize: 20),
                       decoration: const InputDecoration(
                         enabled: true,
                         border: InputBorder.none,
