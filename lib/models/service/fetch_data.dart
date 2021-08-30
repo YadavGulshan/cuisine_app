823339c56ad6c63b3c6fa07beeba7b389c241c41
YadavGulshan <yadavgulshan542@gmail.com>
2021-08-30 13:12:17 +0530

    Fixed fetch data returning dynamic data

---
+++ lib/models/service/fetch_data.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:cuisine_app/models/restaurant.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<RestaurantModel>> fetchData() async {
+  http.Response response = await http.get(Uri.parse("uri"));
+  if (response.statusCode == 200) {
+    Map restaurantData = jsonDecode(response.body);
+    List<dynamic> restaurant = restaurantData["data"];
+    // Return the the json with our model.
+    return restaurant.map((json) => RestaurantModel.fromJson(json)).toList();
+  } else {
+    throw Exception("Something gone wrong ${response.statusCode}");
+  }
+}
