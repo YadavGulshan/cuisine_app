b004b42833f0ebbadc5a5f280f9b67e76e7a16ca
YadavGulshan <yadavgulshan542@gmail.com>
2021-09-09 17:46:55 +0530

    UI rectification

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,5 +1,6 @@
 import 'package:cuisine_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -18,18 +19,8 @@ class LoginPage extends StatelessWidget {
             ),
           ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // SafeArea(
-              //   child: Container(
-              //     height: screen.height * 0.35,
-              //     decoration: const BoxDecoration(
-              //         image: DecorationImage(
-              //             image: AssetImage("assets/burger.jpg"),
-              //             fit: BoxFit.cover)),
-              //   ),
-              // ),
               SafeArea(
                 child: Container(
                   height: screen.height * 0.45,
@@ -40,8 +31,6 @@ class LoginPage extends StatelessWidget {
                   ),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(14),
-                    // color: Colors.white,
-                    // border: Border.all(width: 0.1)
                   ),
                   child: Form(
                     key: _formKey,
@@ -52,87 +41,102 @@ class LoginPage extends StatelessWidget {
                         Center(
                           child: Padding(
                             padding: const EdgeInsets.only(
-                                bottom: 14, left: 14, top: 4, right: 14),
+                                bottom: 4, left: 14, top: 4, right: 14),
                             child: Text(
                               "Login",
                               style: Theme.of(context).textTheme.headline1,
                             ),
                           ),
                         ),
-                        textfield("Email", "Please enter your Email", "Email"),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 14,
+                            right: 14,
+                          ),
+                          child: TextFormField(
+                            style: const TextStyle(fontSize: 20),
+                            obscureText: false,
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.all(10),
+                              enabled: true,
+                              label: Text("Email"),
+                              alignLabelWithHint: true,
+                              hintText: "Email",
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: primaryColor, width: 2.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.black, width: 1.0),
+                              ),
+                            ),
+                            validator: (value) =>
+                                EmailValidator.validate(value!)
+                                    ? null
+                                    : "Please enter a valid email",
+                          ),
+                        ),
                         textfield("Password", "Please enter your password",
-                            "Password"),
+                            "Password", true),
                         Align(
                           alignment: Alignment.centerRight,
                           child: Padding(
-                            padding: const EdgeInsets.only(
-                              left: 14,
-                              right: 14,
-                            ),
-                            child: TextButton(
-                                style: ButtonStyle(
-                                  foregroundColor: MaterialStateProperty.all(
-                                      Colors.red[300]),
-                                  padding: MaterialStateProperty.all(
-                                    const EdgeInsets.all(0),
-                                  ),
-                                ),
-                                onPressed: () {},
-                                child: const Text("Forget password?")),
-                          ),
+                              padding: const EdgeInsets.only(
+                                right: 14,
+                              ),
+                              child: InkWell(
+                                  onTap: () {},
+                                  child: const Text("Forget password?"))),
                         ),
                         Center(
-                          child: Padding(
-                            padding: const EdgeInsets.only(bottom: 5),
-                            child: InkWell(
-                              onTap: () {
-                                if (_formKey.currentState!.validate()) {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text('Processing Data')),
-                                  );
-                                }
-                              },
-                              child: Container(
-                                height: screen.height * 0.055,
-                                width: screen.width * 0.4,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: primaryColor,
-                                  // color: Color(0xFFefefef),
-                                ),
-                                child: const Center(
-                                  child: Text(
-                                    "login",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 19,
-                                    ),
+                          child: InkWell(
+                            onTap: () {
+                              if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Processing Data')),
+                                );
+                              }
+                            },
+                            child: Container(
+                              height: screen.height * 0.065,
+                              width: screen.width * 0.4,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: primaryColor,
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  "login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 19,
                                   ),
                                 ),
                               ),
                             ),
                           ),
                         ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Don't have a account? ",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            InkWell(
+                                onTap: () {},
+                                child: const Text(
+                                  "Sign Up",
+                                  style: TextStyle(color: primaryColor),
+                                ))
+                          ],
+                        ),
                       ],
                     ),
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Don't have a account?",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Sign Up",
-                        style: TextStyle(color: Colors.blue),
-                      ))
-                ],
               ),
             ],
           ),
@@ -141,7 +145,8 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Padding textfield(String hint, String validatorString, String label) {
+  Padding textfield(
+      String hint, String validatorString, String label, bool isobsecure) {
     return Padding(
       padding: const EdgeInsets.only(
         left: 14,
@@ -149,7 +154,7 @@ class LoginPage extends StatelessWidget {
       ),
       child: TextFormField(
         style: const TextStyle(fontSize: 20),
-        obscureText: true,
+        obscureText: isobsecure,
         decoration: InputDecoration(
           contentPadding: const EdgeInsets.all(10),
           enabled: true,
@@ -157,7 +162,7 @@ class LoginPage extends StatelessWidget {
           alignLabelWithHint: true,
           hintText: hint,
           focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.greenAccent, width: 5.0),
+            borderSide: BorderSide(color: primaryColor, width: 2.0),
           ),
           enabledBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.black, width: 1.0),
